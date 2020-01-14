2024aafe9d2dd66bdc66d953a164f05e23bb0749
Nikojoel <niko-holopainen@hotmail.com>
2020-01-14 12:31:56 +0200

    component steps

---
+++ MyApp/components/ListItem.js
@@ -0,0 +1,42 @@
+import {Image, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import React from "react";
+import PropTypes from "prop-types";
+
+const ListItem = (props) => {
+  console.log(props);
+  return (
+    <TouchableOpacity style={styles.background}>
+      <Image
+        style={{ width: 100, height: null, flex: 1 }}
+        source={{uri: props.singleMedia.thumbnails.w160}}
+      />
+      <View style={{ flex: 1, marginLeft: 5}}>
+        <Text style={{fontSize: 30, fontWeight: "bold"}}>{props.singleMedia.title}</Text>
+        <Text style={{fontSize: 20}}>{props.singleMedia.description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    marginTop: 40,
+  },
+  background: {
+    backgroundColor: '#8b8b8b',
+    marginBottom: 10,
+    flexDirection: 'row',
+    padding: 10,
+  },
+});
+
+ListItem.propTypes = {
+  singleMedia: PropTypes.object,
+};
+
+export default ListItem;
