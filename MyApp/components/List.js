83ec947a10d14cfd9e1573028c5f3294da048ed1
Nikojoel <niko-holopainen@hotmail.com>
2020-01-15 10:47:34 +0200

    data handling moved from mediacontext to list

--- MyApp/components/List.js
+++ MyApp/components/List.js
@@ -1,11 +1,19 @@
-import React, {useContext} from "react";
+import React, { useContext, useEffect } from "react";
 import {FlatList} from "react-native";
 import ListItem from "./ListItem";
 import PropTypes from "prop-types";
 import { MediaContext } from "../contexts/MediaContext";
+import { getAllMedia } from "../hooks/APIHooks";
 
 const List = () => {
   const [media, setMedia] = useContext(MediaContext);
+  const [data, loading] = getAllMedia();
+
+  useEffect(() => {
+    console.log(data);
+    setMedia(data);
+  }, [loading]);
+  
   return (
     <FlatList
       data={media}
--- MyApp/contexts/MediaContext.js
+++ MyApp/contexts/MediaContext.js
@@ -1,17 +1,10 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState }  from 'react';
 import PropTypes from 'prop-types';
-import { getAllMedia } from "../hooks/APIHooks";
 
 const MediaContext = React.createContext([{}, media => {}]);
 
 const MediaProvider = (props) => {
   const [media, setMedia] = useState();
-  const [data, loading] = getAllMedia();
-
-  useEffect(() => {
-    console.log(data);
-    setMedia(data);
-  }, [loading]);
 
   return (
     <MediaContext.Provider value={[media, setMedia]}>
